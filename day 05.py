fdcf623078910ce5fe21e5080d417ebab6cdb708
Martin <longmatys@gmail.com>
2024-12-05 21:45:55 +0100

    Day 05 Complete

--- day 05.py
+++ day 05.py
@@ -1,14 +1,14 @@
 import re
-print(1)
+
 def test_run(run,prohibits):
+    #Return either None or tuple index of violating number indexes
     fail = {'sets':set(),'appears':{}}
-    
-    for i,dig in enumerate(run):
+    for i,dig in enumerate(run):        
         if dig in fail['sets']:
-            print(run,dig,fail['appears'])
-            return (fail['appears'].get(dig),i)
+            x = run.index(prohibits['sets_straight'][dig].intersection(run[:i]).pop())
+            return (x,i)
         if not fail['appears'].get(dig):
-            fail['appears'][dig] = i
+            fail['appears'][dig] = i 
         fail['sets'] = fail['sets'].union(prohibits['sets'].get(dig,set()))        
     return None
 def swap(run,res):
@@ -18,24 +18,29 @@ def swap(run,res):
     return run
     ""
 def part2(runs,prohibits):
+    ret = 0
     for run in runs:
         res = test_run(run,prohibits)
-        print('zde',res)
+        
         if res is None:
             continue
         while res:=test_run(run,prohibits):
             #print(res)
             run = swap(run,res)
-        print(run)
+        ret+=run[int(len(run)/2)]
+    print('Part 2:',ret)
 def part1(runs,prohibits):
     
         
-    print(sum([ run[int(len(run)/2)] for run in runs if test_run(run,prohibits)==None]))
-        
+    print('Part 1:',sum([ run[int(len(run)/2)] for run in runs if test_run(run,prohibits)==None]))
+def update_set(set_v,k,v):
+    proh_x = set_v.get(k,set())
+    proh_x.add(v)
+    set_v[k] = proh_x
 def go():
     rules = []
     runs = []
-    prohibits = {'sets':{},'appears':{}}
+    prohibits = {'sets':{},'sets_straight':{}}
     with open('day 05.input.txt') as f:
         phase=1
         for line in f.readlines():
@@ -46,10 +51,12 @@ def go():
             if phase==1:
                 m = re.match(r"(\d+)\|(\d+)",line)
                 if m:
+                    
                     rules.append((int(m.group(1)),int(m.group(2))))
-                    proh_x = prohibits['sets'].get(rules[-1][1],set())
-                    proh_x.add(rules[-1][0])
-                    prohibits['sets'][rules[-1][1]] = proh_x
+                    
+                    
+                    update_set(prohibits['sets'],rules[-1][1],rules[-1][0])
+                    update_set(prohibits['sets_straight'],rules[-1][0],rules[-1][1])
                     
             else:
                 runs.append([int(part) for part in line.split(',')])
